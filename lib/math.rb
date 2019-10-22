65ea0e0edb666bf508eccef12f5eb487e0b9dc1e
Saima Rahman <rahman.saima92@gmail.com>
2019-10-22 17:53:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+return num2 + num1
 end
 
 def subtraction(num1, num2)
-
+  return num2 - num1
 end
 
 def division(num1, num2)
-
+ return num2/num1
 end
 
 def multiplication(num1, num2)
-
+  return num2 * num1
 end
 
 def modulo(num1, num2)
-
+  retunr num2 % num1
 end
 
 def square_root(num)
-
+return Math.sqrt(num)
 end
